43571c2d359018c7a6d075f9b13d9aa1dce43534
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2025-06-11 23:20:54 +0530

    Time: 2 ms (81.65%), Space: 44.3 MB (99.33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int n = ransomNote.length();
-        int m = magazine.length();
-        char[] ch1 = ransomNote.toCharArray();
-        char[] ch2 = magazine.toCharArray();
         int[] hash = new int[26];
 
-        for (int i = 0; i < m; i++) {
-            hash[ch2[i] - 'a']++;
+        for (char c: magazine.toCharArray()) {
+            hash[c - 'a']++;
         }
-        for (int i = 0; i < n; i++) {
-            hash[ch1[i] - 'a']--;
-            if (hash[ch1[i] - 'a'] < 0) {
+        for (char c: ransomNote.toCharArray()) {
+            if (hash[c - 'a'] == 0){
                 return false;
             }
+            hash[c -'a']--;
         }
         return true;
     }
