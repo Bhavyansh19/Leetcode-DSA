62a035d6289aad0f6670250970a2b260ceb63347
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-08-18 21:23:12 +0530

    Time: 919 ms (54.08%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+select name, coalesce(sum(rides.distance),0) as travelled_distance
+from users
+left join
+rides
+on users.id = rides.user_id
+group by users.id, name
+order by travelled_distance desc, name asc;
