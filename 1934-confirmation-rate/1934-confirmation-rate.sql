5864a046dc1694be94fcfe57f681cbe374b09b65
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-09-04 22:22:04 +0530

    Time: 686 ms (72.66%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,18 +1,6 @@
-With CTE1
-as
-(
-    Select user_id,
-    round(SUM(IF(action='confirmed',1,0)*1.0)/count(*),2)
-    as confirmation_rate
-    from Confirmations
-    group by user_id
-),
-CTE2 as
-(Select Signups.user_id, 0 as confirmation_rate
-from Signups
-left Join Confirmations
-on Signups.user_id= confirmations.user_id
-where action is null)
-Select * from CTE1
-union all
-Select * from CTE2;
+select s.user_id,
+round(avg(if(c.action = 'confirmed',1,0)),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
