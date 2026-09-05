ffef73fd4735eeca43637c90cc6a523a3e1c6776
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-09-05 12:26:55 +0530

    Time: 393 ms (57.26%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,14 +1,5 @@
-with res as 
-(
-    select query_name,
-    rating/position as score,
-    case when rating<3 then 1
-    else 0 end as poor_query
-    from Queries
-)
-
 select query_name,
-round(avg(score), 2) as quality,
-round(avg(poor_query)*100.0, 2) as poor_query_percentage
-from res
+round(avg(rating/ position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end)*100/count(8),2) as poor_query_percentage
+from queries
 group by query_name;
