df06085f546315235e89bcd2be4c8e22c4bd2938
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-09-14 20:41:48 +0530

    Time: 339 ms (58.26%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,13 @@
 select id, count(id) as num
 from
-(select requester_id as id
-from RequestAccepted
-union all
-select accepter_id as id
-from RequestAccepted) as res
+(
+    select requester_id as id
+    from RequestAccepted
+    
+    union all
+    select accepter_id as id
+    from RequestAccepted
+) as res
 group by id
 order by num desc
 limit 1;
