7c268aad3875a307d8ddbd24b4ce02270502c1fc
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-09-15 20:57:24 +0530

    Time: 862 ms (70.56%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,9 @@
 select person_name
-from 
-(select *, sum(weight) over(order by turn) as running_sum 
-from queue)
-sub_query
+from
+(
+    select *, sum(weight) over(order by turn) as running_sum
+    from Queue
+) sub_query
 where running_sum <= 1000
 order by turn desc
 limit 1;
